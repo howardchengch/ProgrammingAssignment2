5aaa4778d143765c01b80ed52edca9ed20174bfd
Howard <howard.cheng.ch@gmail.com>
2025-02-28 17:53:41 +0800

    Commit cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,40 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inver <- NULL
+  
+  set <- function(y){
+    x <<- y
+    inver <<- NULL
+  }
+  
+  get <- function() x
+  
+  setInverse <- function(inver) inver <<- inver
+  getInverse <- function() inver
+  
+  return(list(set = set,
+              get = get,
+              setInverse = setInverse,
+              getInverse = getInverse))
 }
 
 
 ## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  if(class(x) == "list"){
+    # Note, it uses list element's name to access the element
+    inver <- x$getInverse()
+    
+    if(is.null(inver)){
+      inver <- solve(x$get(),...)
+      x$setInverse(inver)
+    }
+    
+    return(x$getInverse())
+    
+  }
+  else
+    stop("Invalid class: ", class(x))
 }
